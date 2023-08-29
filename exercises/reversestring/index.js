822654d89bd213d73c595872393d61bd71562a15
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-29 11:21:38 +0300

    :construction:reversestring exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// solution 1 - use the reverse keyword
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+// solution 2 - make use of the for loop
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str){
+//         reversed = character + reversed
+//     }
+//     return reversed;
+// }
+
+// solution 3 - reduce helper
+// function reverse(str) {
+//     return str.split('').reduce((reversed, character) => character + reversed, '');
+// }
 
 module.exports = reverse;
