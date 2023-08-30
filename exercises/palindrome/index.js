2ab2fbf8cf96efffe840b28431671f5d5cfca9fe
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-30 08:22:24 +0300

    :white_check_mark: palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//solution 1 - using reverse and join
+function palindrome(str) {
+    return str === str.split('').reverse().join('')
+}
+
+//solution 2 - every() array helper method
+// function palindrome(str) {
+//     return str.split('').every((char, i) => {
+//         return char === str[str.length - i - 1]
+//     })
+// }
 
 module.exports = palindrome;
