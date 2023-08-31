418a4d0c60a9c184d371e3284d846087ace02b4d
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-31 09:20:26 +0300

    :white_check_mark: max characters in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {}
+    //helper variables
+    let max = 0
+    let maxChar = '';
+    // character map
+    for(let char of str){
+        chars[char] = chars[char] + 1 || 1;
+    }
+    // new lop to iterate through the char map to find the max char
+    for(let char in chars){
+        if (chars[char] > max){
+            max = chars[char]
+            maxChar = char
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
